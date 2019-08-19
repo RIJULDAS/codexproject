d928bb4e341de774ae5c22a061fd35b985d580ac
RIJULDAS <53518611+RIJULDAS@users.noreply.github.com>
2019-08-19 20:30:07 +0530

    Add files via upload

---
+++ Question5.java
@@ -0,0 +1,31 @@
+ import java.io.*;
+ class Question5
+ {
+ 
+    public static void main(String[] args)
+    {
+        
+        
+              long c = 21;
+              while (1==1)
+    {
+             boolean mul = true;
+          for (int i = 1; i < 21; i++)
+    {
+        mul = true;
+        if ( c % i != 0 )
+        {
+            mul = false;
+            break;
+        }
+    }
+       if ( mul )
+    {
+        System.out.println( c );
+        System.exit(21);
+    }
+
+    c++;
+}
+}
+}
