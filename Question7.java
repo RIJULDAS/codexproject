a8d151d50de02634b8887b153fdf5d24fa2ee725
RIJULDAS <53518611+RIJULDAS@users.noreply.github.com>
2019-08-23 13:40:02 +0530

    Update Question7.java

--- Question7.java
+++ Question7.java
@@ -2,16 +2,13 @@ class Question7
 {
      static boolean isPrime(long i)
     {
-        if(i<=1)
-            return false;
-        if(i<=3)
+        if(i==2)
             return true;
-        if(i%2==0 || i%3==0)
-            return  false;
-          
-              for(int j=5; j*j<=i; j+=6)
+        if(i%2==0)
+            return false;
+           for(int j=3; j*j<=i; j++)
         {
-            if(i%j==0 || i%(j+2)==0)
+            if(i%j==0)
                 return false;
             }
         return true;
@@ -19,14 +16,18 @@ class Question7
     public static void main(String args[])
     {
         int count=1;
-        long i=1, lp=-1;
-        while(count<=10001){
+        long i=2, lp=-1;
+        while(count<=10001)
+        {
+             if(i%2!=0)
+             {
             if(isPrime(i))
             {
                 lp=i;
                 count++;
             }
             i++;
+             }
         }
         System.out.println(lp);
     }
